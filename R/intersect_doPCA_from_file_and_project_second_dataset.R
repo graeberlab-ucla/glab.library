24639ce8f9d9af500ae9dc7b676f879648bdd456
1014violin <1014violin@gmail.com>
2023-01-31 15:29:53 -0800

    fixed intersect_doPCA_from_file_project_second_dataset script

--- R/intersect_doPCA_from_file_and_project_second_dataset.R
+++ R/intersect_doPCA_from_file_and_project_second_dataset.R
@@ -18,45 +18,46 @@
 
 intersect_doPCA_from_file_and_project_second_dataset=function(file,file2,train_string,center=TRUE,scale=FALSE,fread=F) {
 
-  requireNamespace(data.table)
+  require(data.table)
 
   if(fread==T){
     data1 = fread(file)
-    data1 = data[rowSums((data1[, -1,with=F] == 0)) < ncol(data1[-1]), ] #remove genes with no variance
+    data1 = data1[rowSums((data1[, -1,with=F] == 0)) < ncol(data1[-1]), ] #remove genes with no variance
     data2 = fread(file2)
     #data2 = data2[rowSums((data2[, -1] == 0)) < ncol(data2[-1]), ] #remove genes with no variance
 
     data1 = data1[!duplicated(data1[,1,with=F]), ]
     data2 = data2[!duplicated(data2[,1,with=F]), ]
 
-    common.genes = intersect_all(data[,1,with=F], data2[,1,with=F])
-    data1 = data1[data1[,1,with=F] %in% common.genes, ]
+    common.genes = glab.library::intersect_all(data1[,1,with=F], data2[,1,with=F])
+    data = data1[data1[,1,with=F] %in% common.genes, ]
     data2 = data2[data2[,1,with=F] %in% common.genes, ]
-    data1 = data1[order(data1[,1,with=F]), ]
+    data = data[order(data[,1,with=F]), ]
     data2 = data2[order(data2[,1,with=F]), ]
-
-    rownames(data1) = make.names(data1[, 1], unique=TRUE)
-    t.data = data.frame(t(data1[, -1,with=F]))
+    data<-data.frame(data)
+    rownames(data) = make.names(data[, 1], unique=TRUE)
+    data<-as.data.table(data)
+    t.data = data.frame(t(data[, -1,with=F]))
   }
   else{
-  data1=read.delim(file, header = T, stringsAsFactors = F)
-  data2=read.delim(file2, header = T, stringsAsFactors = F)
-  data1 = data1[!duplicated(data1[,1]),]#needed if data has duplicates
-  data2 = data2[!duplicated(data2[,1]),]
-  #remove rows that are all 0
-  data1 = data1[rowSums((data1[,-1]==0))<ncol(data1[-1]),]
+    data1=read.delim(file, header = T, stringsAsFactors = F)
+    data2=read.delim(file2, header = T, stringsAsFactors = F)
+    data1 = data1[!duplicated(data1[,1]),]#needed if data has duplicates
+    data2 = data2[!duplicated(data2[,1]),]
+    #remove rows that are all 0
+    data1 = data1[rowSums((data1[,-1]==0))<ncol(data1[-1]),]
 
-  common.genes <-intersect((data1[,1]), (data2[,1]))
-  data <-data1[(data1[,1]) %in% common.genes,]
-  data2 <-data2[(data2[,1]) %in% common.genes,]
-  data = data[order(data[,1]), ]
-  data2 = data2[order(data2[,1]), ]
-  # data=data1[match(common.genes,data[,1]),]
-  # data2=data2[match(common.genes,data2[,1]),]
+    common.genes <-intersect((data1[,1]), (data2[,1]))
+    data <-data1[(data1[,1]) %in% common.genes,]
+    data2 <-data2[(data2[,1]) %in% common.genes,]
+    data = data[order(data[,1]), ]
+    data2 = data2[order(data2[,1]), ]
+    # data=data1[match(common.genes,data[,1]),]
+    # data2=data2[match(common.genes,data2[,1]),]
 
 
-  t.data=t(data[,-1])
-  #t.data = t(data) #if genenames inrownames
+    t.data=t(data[,-1])
+    #t.data = t(data) #if genenames inrownames
   }
 
   pca<-prcomp(t.data,scale=scale,center=center)
